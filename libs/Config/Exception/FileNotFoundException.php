eda482bc00d01ac54b07759e7ab5b329882fffd0
Harald Lapp <h.lapp@clipdealer.de>
2020-07-08 17:13:46 +0200

    initial commit exception classes

---
+++ libs/Config/Exception/FileNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the 'octris/config' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Config\Exception;
+
+class FileNotFoundException extends FileException {
+}
---
+++ libs/Config/Exception/FormatException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the 'octris/config' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Config\Exception;
+
+use Octris\Config\RuntimeException;
+
+class FormatException extends RuntimeException {
+}
